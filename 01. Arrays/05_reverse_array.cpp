6447cf2fa68b3bb3dec3d5d14b7f3fa7de550f5d
afkniladri <niladri1406@gmail.com>
2021-10-08 23:00:04 +0530

    array : reverse array

---
+++ 01. Arrays/05_reverse_array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int n){
+    int low = 0, high = n - 1, mid;
+    while(low < high){
+        swap(arr[low],arr[high]);
+        low ++;
+        high --;
+    }
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6};
+    int n = sizeof(arr)/sizeof(int);
+
+    reverseArray(arr,n);
+
+    // print array
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
+
+    return 0;
+}
