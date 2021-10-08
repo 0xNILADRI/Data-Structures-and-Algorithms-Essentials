7f8667bc7903da3f276e04106968bf8493f52ce1
afkniladri <niladri1406@gmail.com>
2021-10-08 22:29:14 +0530

    array : pass array to function (by value)

---
+++ 01. Arrays/02_pass_array_to_function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int * arr, int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<endl;
+    }
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6};
+    int n = sizeof(arr)/sizeof(int);
+
+    printArray(arr,n);
+    return 0;
+}
