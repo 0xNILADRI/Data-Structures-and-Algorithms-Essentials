b24d98c6a3843111dd6e56265d9db30dcd0749ca
afkniladri <niladri1406@gmail.com>
2021-10-09 14:46:47 +0530

    array : reverse array ; code optimized

--- 01. Arrays/05_reverse_array.cpp
+++ 01. Arrays/05_reverse_array.cpp
@@ -2,11 +2,9 @@
 using namespace std;
 
 void reverseArray(int arr[], int n){
-    int low = 0, high = n - 1, mid;
+    int low = 0, high = n - 1;
     while(low < high){
-        swap(arr[low],arr[high]);
-        low ++;
-        high --;
+        swap(arr[low++],arr[high--]);
     }
 }
 
