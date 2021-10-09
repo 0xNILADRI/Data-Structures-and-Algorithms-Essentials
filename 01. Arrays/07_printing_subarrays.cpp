097e477e64ceb9b106fa8f9af535c0d7ac4ecb76
afkniladri <niladri1406@gmail.com>
2021-10-09 14:15:37 +0530

    printing subarrays

--- 01. Arrays/07_printing_subarrays.cpp
+++ 01. Arrays/07_printing_subarrays.cpp
@@ -2,20 +2,15 @@
 using namespace std;
 
 void printSubarrays(int arr[], int n){
-    int sum=0, sumMax=0;
     for(int i=0; i<n; i++){
         for(int j=i; j<n; j++){
             for(int k=i; k<=j; k++){
                 cout<<arr[k]<<",";
-                sum += arr[k];
             }
             cout<<endl;
         }
-        cout<<"SUM : "<<sum<<endl;
-        if(sum>sumMax) sumMax = sum;
-        sum = 0;
+        cout<<endl;
     }
-    cout<<endl<<"Largest Sum : "<<sumMax<<endl;
 }
 
 int main(){
