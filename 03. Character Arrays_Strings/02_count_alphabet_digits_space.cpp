dd72c716a493935e4d8c2458ad5576cd7794d214
afkniladri <niladri1406@gmail.com>
2021-10-16 20:14:57 +0530

    string : count alphanumeric, digits and spaces

---
+++ 03. Character Arrays_Strings/02_count_alphabet_digits_space.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    int alphaCount=0, spaceCount=0, digitCount=0;
+    ch = cin.get(); // will record the first enter and won't interfere with our original string
+    while(ch!='\n'){
+        if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')){
+            alphaCount++;
+        }
+        else if(ch>='0' and ch<='9'){
+            digitCount++;
+        }
+        else if(ch==' ' or ch=='\t'){
+            spaceCount++;
+        }
+        ch = cin.get();
+    }
+    cout<<"Aplhanumeric Count : "<<alphaCount<<endl;
+    cout<<"Digit Count : "<<digitCount<<endl;
+    cout<<"Space Count : "<<spaceCount<<endl;
+    return 0;
+}
