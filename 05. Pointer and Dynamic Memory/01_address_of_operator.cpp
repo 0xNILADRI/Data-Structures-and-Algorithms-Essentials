813f0a2e4e6cd0512c2ac83acd4c904b7b8f59b2
afkniladri <niladri1406@gmail.com>
2021-10-22 19:57:25 +0530

    Pointer and Dynamic Memory : address of operator

---
+++ 05. Pointer and Dynamic Memory/01_address_of_operator.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 10;
+    cout<<"Address of x : "<<&x<<endl;      // using & operator (address of operator) for address of variable
+    return 0;
+}
