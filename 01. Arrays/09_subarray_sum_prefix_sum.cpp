9e4a08d6db3234ccc690c7978ad1bc439a7206a0
Niladri Ghosh <niladri1406@gmail.com>
2023-12-25 09:42:13 +0530

    refactor : prettified the verbose

--- 01. Arrays/09_subarray_sum_prefix_sum.cpp
+++ 01. Arrays/09_subarray_sum_prefix_sum.cpp
@@ -27,8 +27,9 @@ int main(){
     int n = sizeof(arr)/sizeof(int);
 
     // print array 
+    cout<<"Array Elements : "<<endl;
     for(int x: arr){
-        cout<<x<<endl;
+        cout<<x<< " ";
     }
     
     cout<<endl<<"Largest Subarray Sum : "<<subarraySum(arr,n)<<endl;;
